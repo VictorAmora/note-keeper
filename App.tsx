881fbb247e94a5a1b996053725268864f1ba1940
VictorAmora <joao.victor@teclat.com.br>
2023-03-20 16:14:42 -0300

    inicio da interface

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, TextInput, View, Image, FlatList, SafeAreaView, } from 'react-native';
+import TextInputMask from 'react-native-text-input-mask'
 export default function App() {
+  const mask = '+1 ([000]) [000]-[0000]';
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={{ padding: 130, paddingTop: 30, alignItems: 'center', justifyContent: 'space-between' }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Image style={{ width: 50, height: 50, borderRadius: 20, paddingLeft: 10 }} source={require('./assets/lupa.png')} />
+        </View>
+        <View style={{ position: 'absolute', bottom: 0, alignSelf: 'center', alignItems: 'center' }}>
+          <SafeAreaView style={styles.container}>
+            <Text>testando</Text>
+          </SafeAreaView>
+
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -15,6 +26,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
