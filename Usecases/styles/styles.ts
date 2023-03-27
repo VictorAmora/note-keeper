d8d62e15eb701361997243b0c902bafb222c4cf0
VictorAmora <joao.victor@teclat.com.br>
2023-03-27 15:53:54 -0300

    ajuste do codigo da navegação

---
+++ Usecases/styles/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
+    mainView: {
+
+        paddingTop: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    searchBox: {
+        alignSelf: 'center',
+        flexDirection: 'row',
+        borderColor: "green",
+        borderStyle: 'solid',
+        borderRadius: 50,
+        borderWidth: 2,
+        width: 300,
+        height: 50,
+
+    }
+});
